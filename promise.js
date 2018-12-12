7109922eef09e0fe31986a15a53ea6a44cfc4d21
tidy.github.io <593243536@qq.com>
2018-12-12 14:03:16 +0800

    '1'

--- promise.js
+++ promise.js
@@ -10,29 +10,31 @@
  * 优势：比传统的解决方案——回调函数和事件，更合理和更强大
  * 
  */
+/**
+ * promise 
+ */
 
 /**
  *then
- *promise.then(undefined, onRejected) 
+ *promise.then(onFulfilled, onRejected);
  */
 // var promise = new Promise(function (resolve, reject) {
-//   resolve("传递给then的值");
+//   reject("传递给then的值");
 // });
 // promise.then(function (value) {
 //   console.log('value:', value);
-// }
+// }, function (err) {
+//   console.log('err:', err)
+// })
 
 /**
  *catch
  *promise.then(undefined, onRejected) 
  */
 // var promise = new Promise(function (resolve, reject) {
-//   // resolve("传递给then的值");
 //   reject("传递给catch的值");
 // });
-// promise.then(function (value) {
-//   console.log('value:', value);
-// }).catch(function (error) {
+// promise.catch(function (error) {
 //   console.error('error:', error);
 // });
 
@@ -41,25 +43,25 @@
  * 根据接收到的参数不同，返回不同的promise对象  
  * 大概分为三类:
  * -接收到promise对象参数的时候: 返回的还是接收到的promise对象
- * -接收到thenable类型的对象的时候: 返回一个新的promise对象，这个对象具有一个 then 方法
+ * -接收到thenable类型（有then方法）的对象的时候: 返回一个新的promise对象，这个对象具有一个 then 方法 (ajax)
  * -接收的参数为其他类型的时候（包括JavaScript对或null等）: 返回一个将该对象作为值的新promise对象
  */
 
-var taskName = "task 1";
-var obj = {
-  "name": "cozy"
-};
-asyncTask(JSON.stringify(obj)).then(function (result) {
-  console.log("result:", result)
-}).catch(function (error) {
-  console.log("error:", error)
-})
+// var taskName = "task 1";
+// var obj = {
+//   "name": "cozy"
+// };
+// asyncTask(JSON.stringify(obj)).then(function (result) {
+//   console.log("result:", result)
+// }).catch(function (error) {
+//   console.log("error:", error)
+// })
 
-function asyncTask(name) {
-  return Promise.resolve(name).then(function (result) {
-    return "Done!" + result
-  })
-}
+// function asyncTask(name) {
+//   return Promise.resolve(name).then(function (result) {
+//     return "Done!" + result
+//   })
+// }
 
 /**
  * Promise.reject(object)
@@ -75,7 +77,7 @@ function asyncTask(name) {
  * 如果参数中的任何一个promise为reject的话，则整个Promise.all调用会立即终止，并返回一个reject的新的promise对象。
  */
 // var promise = new Promise(function (resolve, reject) {
-//   setTimeout(reject(1), 1)
+//   setTimeout(reject(1), 1000)
 // })
 // var p1 = promise.then(function (value) {
 //   return value
@@ -85,7 +87,7 @@ function asyncTask(name) {
 // Promise.all([p1, p2, p3]).then(function (results) {
 //   console.log(results);  // [1, 2, 3]
 // }).catch(function (error) {
-//   console.log(error)
+//   console.log('error:', error)
 // });
 
 
@@ -94,22 +96,24 @@ function asyncTask(name) {
  * 生成并返回一个新的promise对象。
  * 参数 promise 数组中的任何一个promise对象如果变为resolve或者reject的话，该函数就会返回，并使用这个promise对象的值进行resolve或者reject。
  */
-// var promise = new Promise(function (value) {
-//   setTimeout(resolve(1), 1000)
-// })
-// var p1 = promise
-//   .then(function (result) {
-//     return result
-//   })
-//   .catch(function (error) { }),
-//   p2 = Promise.resolve(2),
-//   p3 = Promise.resolve(3);
+var promise = new Promise(function (value) {
+  setTimeout(resolve(1), 1000)
+})
+var p1 = promise
+  .then(function (result) {
+    return result
+  })
+  .catch(function (error) {
+    return error
+  }),
+  p2 = Promise.resolve(2),
+  p3 = Promise.resolve(3);
 
-// Promise.race([p1, p2, p3])
-//   .then(function (results) {
-//     console.log('results:', results);
-//   })
-//   .catch(function (error) {
-//     console.log('error:', error)
-//   })
+Promise.race([p1, p2, p3])
+  .then(function (results) {
+    console.log('results:', results);
+  })
+  .catch(function (error) {
+    console.log('error:', error)
+  })
 
